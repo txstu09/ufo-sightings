77deb0c48d02b94d49e32cffee5caed9085572cd
txstu09 <sgillies09@gmail.com>
2018-01-30 23:32:17 -0600

    create renderTable func

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ $dtSearchBtn.addEventListener("click", handleSearch);
 
 var filteredSightings = data;
 
+function renderTable() {
+    $tbody.innerHTML = "";
+    for(var x = 0; x < filteredSightings.length; x++) {
+        var sighting = filteredSightings[x];
+        var fields = Object.keys(sighting);
+        var $row = $tbody.insertRow(x);
+        for(var y = 0; y < fields.length; y++) {
+            var field = fields[y];
+            var $cell = $row.insertCell(y);
+            $cell.innerText = sighting[field];
+        }
+    }
+}
