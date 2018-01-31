44b23b203ee42f1a6934decb1e1b7acafc5615d0
txstu09 <sgillies09@gmail.com>
2018-01-30 23:26:51 -0600

    update app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var $tbody = document.querySelector("tbody");
+var $datetimeInput = document.querySelector("#datetime");
+var $SearchBtn = document.querySelector("#search");
+
+$dtSearchBtn.addEventListener("click", handleSearch);
+
+var filteredSightings = data;
+
