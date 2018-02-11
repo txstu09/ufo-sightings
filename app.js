067f614f718236d3625cebe582f5f782b00babd7
txstu09 <sgillies09@gmail.com>
2018-02-10 21:15:17 -0600

    add search fields and reset

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var $tbody = document.querySelector("tbody");
 var $dateInput = document.querySelector("#date");
 var $cityInput = document.querySelector("#city");
+var $stateInput = document.querySelector("#state");
+var $countryInput = document.querySelector("#country");
+var $shapeInput = document.querySelector("#shape");
+
 var $searchBtn = document.querySelector("#search");
+var $resetBtn = document.querySelector("#reset");
 
 $searchBtn.addEventListener("click", handleSearch);
+$resetBtn.addEventListener("click", resetSearch);
 
 var filteredSightings = dataSet;
 
@@ -31,6 +37,15 @@ function handleSearch() {
     },{
         "key":"city",
         "value":$cityInput.value.toLowerCase()
+    },{
+        "key":"state",
+        "value":$stateInput.value.toLowerCase()
+    },{
+        "key":"country",
+        "value":$countryInput.value.toLowerCase()
+    },{
+        "key":"shape",
+        "value":$shapeInput.value.toLowerCase()
     }];
     
     var tableData = dataSet;
@@ -50,4 +65,13 @@ function handleSearch() {
     });
     filteredSightings = tableData;
     renderTable();
+}
+
+function resetSearch() {
+    $dateInput.value = "";
+    $cityInput.value = "";
+    $stateInput.value = "";
+    $countryInput.value = "";
+    $shapeInput.value = "";
+    handleSearch();
 }
